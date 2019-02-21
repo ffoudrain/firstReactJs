b7681f1516792bf5ebe27c860a5eb6a0ba401adb
f.foudrain <f.foudrain@cat-amania.com>
2019-02-21 09:08:57 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-//test 803
+//test push command line
 class App extends Component {
   render() {
     return (
